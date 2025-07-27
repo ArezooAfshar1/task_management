888620b3a814faecd62481135cc6f8d355f8bfa4
arezoo <arezooafshar108@gmail.com>
2025-07-27 22:44:21 +0330

    configure postgress db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ProjectsModule } from './projects/projects.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ProjectsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      username: 'myuser',
+      password: 'mypass',
+      port: 5432,
+      synchronize: true, // دیتابیس با تعغیراتی که ما در entity میدیدم تعغیر کنه 
+      // تو حالت دولوپ اینو فعال میکنیم فقط 
+      database: 'mydb',
+      entities: [__dirname + '/**/entities/*.entity.js']  // گفتیم یه پوشه بیا عقب برو تو اس ار سی هر ماژولی بود رو باز کن برو تو ان تی تیز و دنبال فایلی با این پسوند بگرد 
+    }),
+    
+    ProjectsModule],
   controllers: [],
   providers: [],
 })
--- src/projects/entities/project.entity.ts
+++ src/projects/entities/project.entity.ts
@@ -1,1 +1,13 @@
-export class Project {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Projects {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    //@Column()
+    //status: enum;
+}
